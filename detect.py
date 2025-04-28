83afe76866914bb15f1bf7f4646fd584832d79b2
Ankit Pathak <pathakankit688@gmail.com>
2025-04-28 13:20:26 +0530

    update

--- detect.py
+++ detect.py
@@ -41,7 +41,7 @@ MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList=['Male','Female']
 
-faceNet=cv2.dnn.readNet(faceModel,faceProto)
+faceNet=cv2.dnn.readNet(faceModel,faceProto) 
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
